b63db63f7f8575cbc92adcbaa60f763206e8d598
ricardoTellezA <lian.redi@hotmail.com>
2022-03-21 00:33:27 -0600

    Subiendo el proyecto a heroku

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ const server = new ApolloServer({
 });
 
 //arrancar server
-server.listen().then(({ url }) => {
+server.listen({port: process.env.PORT || 4000}).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
